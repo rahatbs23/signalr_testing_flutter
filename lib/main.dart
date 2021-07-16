3a6a361218a7901ff3f8745f87d6eaf5a424e7cb
Md Ruhul Amin Rahat <ruhul.rahat@bs-23.net>
2021-07-16 20:59:39 +0600

    signalR group msg added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 //import 'package:universal_io/io.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_background/flutter_background.dart';
 import 'package:signalr_core/signalr_core.dart';
+import 'package:wakelock/wakelock.dart';
 //import 'package:http/io_client.dart';
 
 void main() {
@@ -8,7 +12,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,91 +35,164 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
+
+  final androidConfig = FlutterBackgroundAndroidConfig(
+    notificationTitle: "Title of the notification",
+    notificationText: "Text of the notification",
+    notificationImportance: AndroidNotificationImportance.Default,
+    notificationIcon: AndroidResource(name: 'background_icon', defType: 'drawable'), // Default is ic_launcher from folder mipmap
+  );
+
+
+
+  var globalCounter = 0;
 
   @override
   void initState() {
     super.initState();
-    connectToServer();
+
+
+    //Wakelock.enable();
+
+    //_initBackground();
+    _buildServer();
+  }
+
+  _initBackground() async{
+    bool success = await FlutterBackground.initialize(androidConfig: androidConfig);
+    print("background init: "+  success.toString());
+    print("already has permissions: " + FlutterBackground.hasPermissions.toString());
+
+
+    await FlutterBackground.enableBackgroundExecution();
+
+
+    if(!FlutterBackground.isBackgroundExecutionEnabled){
+        print('retrying background');
+        await FlutterBackground.enableBackgroundExecution();
+    }
+
+
+    Timer.periodic(new Duration(seconds: 1), (timer) {
+      //debugPrint(timer.tick.toString());
+      globalCounter++;
+      print('$globalCounter  isBackground:  ${FlutterBackground.isBackgroundExecutionEnabled}');
+    });
   }
 
 
+  var connection;
 
-  connectToServer() async {
-
-    /*final connection = HubConnectionBuilder().withUrl('http://localhost:5000/chatHub',
-        HttpConnectionOptions(
-          client: IOClient(HttpClient()..badCertificateCallback = (x, y, z) => true),
-          logging: (level, message) => print(message),
-        )).build();
-*/
-
-    final connection = HubConnectionBuilder().withUrl('http://localhost:5000/chatHub',
+  _buildServer() async {
+     connection = HubConnectionBuilder().withUrl('http://192.168.216.45:5003/chatHub',
         HttpConnectionOptions(
           logging: (level, message) => print(message),
-        )).build();
+        )).withAutomaticReconnect().build();
 
-    await connection.start();
+
+   /*  this.connection.on("ReceiveMessage", (user, message) => {
+      print(user + " " + message);
+     });*/
+
+
+  /*   connection.on('ReceiveMessage', (user, message) {
+       //print(user);
+       //print(message);
+     });*/
+     connection.on('ReceiveMessage', (message) {
+       print("ReceiveMessage" +  message.toString());
+     });
+
+/*    await connection.start();
 
     connection.on('ReceiveMessage', (message) {
       print(message.toString());
     });
 
-    await connection.invoke('SendMessage', args: ['Bob', 'Says hi!']);
+    await connection.invoke('SendMessage', args: ['Bob', 'Says hi!']);*/
 
   }
 
+  var group = "rahat_group";
+  var groupMessage = "group message";
+  var pMessage = "personal message";
+  var message = "message";
 
-  void _incrementCounter() {
+  _start() async{
+     try {
+       await this.connection.start();
+       print("SignalR Connected.");
+     } catch (err) {
+       print(err);
+       //setTimeout(this.start, 5000);
+     }
+  }
+
+  //send({String? methodName, List<dynamic>? args}) => Future<void>
+  //send({String? methodName, List<dynamic>? args}) => Future<void>
+  //invoke(String, {List<dynamic>? args}) => Future<dynamic>
+
+  _joinGroup() async{
+    await this.connection.invoke('JoinGroup', args: ['habib', this.group]);
+    //await this.connection.invoke('JoinGroup', args: ['habib', this.group]);
+    //await connection.invoke('SendMessage', args: ['Bob', 'Says hi!']);
+
+  }
+
+  _sendToGroup() async{
+    //await this.connection.invoke("GroupMsg", this.group, this.groupMessage);
+    await this.connection.invoke('GroupMsg', args: ['rahat_group', this.groupMessage]);
+  }
+
+  _personalMessage() async{
+    //await this.connection.invoke("PersonalMessage", "habib", this.pMessage);
+    await this.connection.invoke('PersonalMessage', args: ['habib', this.pMessage]);
+  }
+
+  _broadcast() async{
+    //await this.connection.invoke("BroadcastMsg", "habib", this.message);
+    await this.connection.invoke('BroadcastMsg', args: ['habib', 'broadcast msg']);
+  }
+
+  changeText() {
     setState(() {
-      _counter++;
     });
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            ElevatedButton(
+                onPressed: () => changeText(),
+                child: Text('Counter: $globalCounter')),
+            ElevatedButton(
+                onPressed: _start,
+                child: Text('Start')),
+            ElevatedButton(
+                onPressed: _joinGroup,
+                child: Text('Join Group')),
+            ElevatedButton(
+                onPressed: _sendToGroup,
+                child: Text('Send To Group')),
+            ElevatedButton(
+                onPressed: _personalMessage,
+                child: Text('Personal Message')),
+            ElevatedButton(
+                onPressed: _broadcast,
+                child: Text('Broadcast'))
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
